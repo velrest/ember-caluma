10bc5a9ca8344ff94e1104b4d38f80a6743eb06c
Jonas Cosandey <jonas.cosandey@adfinis.com>
2025-02-25 16:04:22 +0100

    wip

--- packages/form/addon/modifiers/autoresize.js
+++ packages/form/addon/modifiers/autoresize.js
@@ -6,9 +6,12 @@ export default class CustomAutoresizeModifier extends AutoresizeModifier {
   @service inViewport;
 
   modify(...args) {
-    super.modify(...args);
+    const resize = args.resize;
+    if (!resize) {
+      super.modify(...args);
 
-    this.inViewport.watchElement(this.el).onEnter(this.resize);
+      this.inViewport.watchElement(this.el).onEnter(this.resize);
+    }
     registerDestructor(this, () => this.inViewport.stopWatching(this.el));
   }
 }
